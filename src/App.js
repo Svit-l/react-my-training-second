ed103996d24478aa202999f46c2c2dbfd09078d1
Svit-l <lov.svit@gmail.com>
2022-01-14 02:29:48 +0200

    рендер с помощью класса, дефолтные свойства

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
-      <Counter />
+      <Counter initialValue={200} />
     </div>
   );
 }
--- src/Counter.js
+++ src/Counter.js
@@ -1,14 +1,93 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+  static defaultProps = {
+    initialValue: 0,
+  };
+  // constructor() {
+  //   super();
+
+  //   this.state = {
+  //     value: 5,
+  //   };
+  // }
+
+  static propTypes = {
+    //
+  };
+
+  state = {
+    value: 6,
+    forSum: this.props.initialValue,
+  };
+
+  handleIncrement = event => {
+    console.log('click +1');
+    console.log(this);
+    console.log(event);
+    console.log(event.target);
+    console.log(event.type);
+    const target = event.target;
+    // const { target } = event;
+
+    setTimeout(() => {
+      this.setState(prevState => ({
+        value: prevState.value + 1,
+      }));
+      console.log(target);
+    }, 1000);
+  };
+
+  // handleDecrement = () => {
+  //   this.setState({
+  //     value: 123,
+  //   });
+  // };
+
+  handleDecrement = () => {
+    // this.setState(prevState => {
+    //   return {
+    //     value: prevState.value - 1,
+    //   };
+    // });
+    this.setState(prevState => ({
+      value: prevState.value - 1,
+    }));
+  };
+
+  handleSum = () => {
+    this.setState(prevState => ({
+      forSum: prevState.forSum + 3,
+    }));
+  };
+
   render() {
     return (
       <div className="Counter">
-        <span className="Counter__value">0</span>
+        <span className="Counter__value">{this.state.value}</span>
+
+        <span className="CounterSum__value">
+          {' '}
+          динамическая переменная {this.state.forSum}
+        </span>
 
         <div className="Counter__controls">
-          <button type="button">Увеличить на 1</button>
-          <button type="button">Уменьшить на 1</button>
+          <button type="button" onClick={this.handleSum}>
+            Вернуть сумму динамич + 3
+          </button>
+          <button type="button" onClick={this.handleIncrement}>
+            Увеличить на 1
+          </button>
+          <button
+            type="button"
+            // onClick={() => {
+            //   console.log('click -1');
+            //   console.log(this);
+            // }}
+            onClick={this.handleDecrement}
+          >
+            Уменьшить на 1
+          </button>
         </div>
       </div>
     );
